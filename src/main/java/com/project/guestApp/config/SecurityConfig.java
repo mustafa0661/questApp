1f9ace9c690a353e2da08ab65414ca3314d09755
Mustafa UZUN <mustafa.uzun1961ts@gmail.com>
2025-05-31 18:16:26 +0300

    Swagger eklendi.
    UserController için swagger eklendi

--- src/main/java/com/project/guestApp/config/SecurityConfig.java
+++ src/main/java/com/project/guestApp/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -36,6 +37,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .requestMatchers("/users/**").hasAnyRole( "ADMIN", "USER")
                         .requestMatchers("/roles/**").hasAnyRole( "ADMIN", "USER")
                         .requestMatchers("/posts/**").hasAnyRole( "ADMIN", "USER")
--- src/main/java/com/project/guestApp/controllers/UserController.java
+++ src/main/java/com/project/guestApp/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.project.guestApp.entities.User;
 import com.project.guestApp.responses.UserResponse;
 import com.project.guestApp.services.RoleService;
 import com.project.guestApp.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Kullanıcı İşlemleri", description = "Kullanıcı oluşturma, listeleme, silme gibi işlemleri içerir.")
 public class UserController {
 
     private final RoleService roleService;
@@ -24,16 +27,28 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Tüm kullanıcıları getir",
+            description = "Sistemdeki tüm kullanıcıları getirir."
+    )
     public List<UserResponse> getAllUsers() {
         return userService.getAllUsers().stream().map(UserResponse::new).toList();
     }
 
     @PostMapping
+    @Operation(
+            summary = "Yeni Bir Kullanıcı Ekle",
+            description = "Sİsteme yeni bir kullanıcı ekler."
+    )
     public User createUser(@RequestBody User newUser) {
         return userService.saveOneUser(newUser);
     }
 
     @GetMapping("/{userId}")
+    @Operation(
+            summary = "Belirli bir kullanıcıyı getir",
+            description = "ID'si verilen kullanıcıyı döndürür."
+    )
     public UserResponse getOneUser(@PathVariable Long userId) {
         //custom exception
         User user = userService.getOneUserById(userId);
@@ -41,16 +56,28 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
+    @Operation(
+            summary = "Kullanıcıyı Güncelle",
+            description = "ID'si verilen kullanıcıyı günceller."
+    )
     public User updateOneUser(@PathVariable Long userId, @RequestBody User newUser) {
         return userService.updateOneUser(userId, newUser);
     }
 
     @DeleteMapping("/{userId}")
+    @Operation(
+            summary = "Kullanıcıyı Sil",
+            description = "ID'si verilen kullanıcıyı siler."
+    )
     public void deleteOneUser(@PathVariable Long userId) {
         userService.deleteOneUser(userId);
     }
 
     @PostMapping("/{userId}/roles")
+    @Operation(
+            summary = "Kullanıcıya Rol Atama",
+            description = "ID'si verilen kullanıcıya rol ataması yapar."
+    )
     public ResponseEntity<?> assignRoleToUser(@PathVariable Long userId, @RequestBody Map<String, String> request) {
         String roleName = request.get("roleName");
         Role role = roleService.getRoleByName(roleName);
